166680475a7bdcff81484dcf0c53452b02d8aa31
Deepak Mishra <deepakmishra@Deepaks-MacBook-Air.local>
2023-09-11 06:17:40 +0530

    added support for product category

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,26 +2,42 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8080/api/products'
+  private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
 
-  constructor(private httpClient : HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-getProductList(categoryId: number) : Observable<Product[]> {
-  const searchUrl = `${this.baseUrl}/search/findProductByCategoryId?id=${categoryId}`;
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+  getProductList(categoryId: number): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/search/findProductByCategoryId?id=${categoryId}`;
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
-    )
+    );
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
   }
 }
 
-interface GetResponse {
-  _embedded : {
-    products : Product[]
+interface GetResponseProducts {
+  _embedded: {
+    products: Product[];
+  }
+}
+
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
